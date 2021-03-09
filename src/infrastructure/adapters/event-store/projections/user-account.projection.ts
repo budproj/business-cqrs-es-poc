6ffd4940bbb6feea353ce3263541c8d5f63f048f
delucca <delucca+notebook@pm.me>
2021-03-09 00:58:49 -0300

    wip

--- src/infrastructure/adapters/event-store/projections/user-account.projection.ts
+++ src/infrastructure/adapters/event-store/projections/user-account.projection.ts
@@ -32,6 +32,14 @@ export class UserAccountProjection implements OnApplicationBootstrap {
   constructor(private readonly eventStore: EventStoreAdapterProvider) {}
 
   public async onApplicationBootstrap() {
+    console.log(String(UserAccountProjection))
+    // Move query string to a method inside this class (we can use String to stringify it afterwards)
+    // Abstract logic to extract aggregateID
+    // Abstract logic to select version handler (with fallback logic)
+    // Create interface to define the most recent (active) version
+    // Stringify everythin with String(this.blabla)
+    // Code upsert logic to eventStore adapter that will check if  the projection query has changed and update it if so
+    // Maybe add a logic to start the projection itself
     // Await this.eventStore.upsertProjection(USER_ACCOUNT_PROJECTION_NAME, USER_ACCOUNT_PROJECTION_QUERY)
   }
 }
