85e610058c18afeb5d81970b414efbb4bdbce153
delucca <delucca+pc@pm.me>
2021-03-11 20:00:32 -0300

    wip

---
+++ src/infrastructure/adapters/event-store/projections/base.projection.ts
@@ -0,0 +1,5 @@
+export abstract class EventStoreProjection {
+  protected sync(): void {
+    console.log('ok')
+  }
+}
--- src/infrastructure/adapters/event-store/projections/user-account.projection.ts
+++ src/infrastructure/adapters/event-store/projections/user-account.projection.ts
@@ -3,43 +3,38 @@ import { Injectable, OnApplicationBootstrap } from '@nestjs/common'
 import { USER_AGGREGATE_NAME } from '@core/modules/user/constants'
 import { EventStoreAdapterProvider } from '@infrastructure/adapters/event-store/event-store.provider'
 
-const USER_ACCOUNT_PROJECTION_NAME = 'user-account'
-const USER_ACCOUNT_PROJECTION_QUERY = `
-  fromCategory('${USER_AGGREGATE_NAME}')
-    .when({
+import { EventStoreProjection } from './base.projection'
+
+@Injectable()
+export class UserAccountProjection extends EventStoreProjection implements OnApplicationBootstrap {
+  protected get resultStreamName() {
+    return 'user-account'
+  }
+
+  protected get query() {
+    return `
+    fromCategory('${USER_AGGREGATE_NAME}')
+      .when({
         $init: () => ({}),
 
         CreatedUser: (state, event) => {
           const { aggregateID } = event.metadata
           const aggregateData = state[aggregateID]
-          const versionHandlers = {
-              1: () => {
-                state[aggregateID] = {
-                    ...aggregateData,
-                    firstName: event.data.firstName,
-                }
-              }
+
+          state[aggregateID] = {
+            ...aggregateData,
+            firstName: event.data.firstName,
           }
-
-          const eventVersionHandler = versionHandlers[event.metadata.version]
-          eventVersionHandler()
         }
-    })
-`
+      })
+  `
+  }
 
-@Injectable()
-export class UserAccountProjection implements OnApplicationBootstrap {
-  constructor(private readonly eventStore: EventStoreAdapterProvider) {}
+  constructor(protected readonly eventStore: EventStoreAdapterProvider) {
+    super()
+  }
 
   public async onApplicationBootstrap() {
-    console.log(String(UserAccountProjection))
-    // Move query string to a method inside this class (we can use String to stringify it afterwards)
-    // Abstract logic to extract aggregateID
-    // Abstract logic to select version handler (with fallback logic)
-    // Create interface to define the most recent (active) version
-    // Stringify everythin with String(this.blabla)
-    // Code upsert logic to eventStore adapter that will check if  the projection query has changed and update it if so
-    // Maybe add a logic to start the projection itself
-    // Await this.eventStore.upsertProjection(USER_ACCOUNT_PROJECTION_NAME, USER_ACCOUNT_PROJECTION_QUERY)
+    this.sync()
   }
 }
